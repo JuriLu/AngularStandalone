f19c4216b0700220add8a6d42664b51187453b1b
JurgenLubonja <jurgen.lubonja@eng.it>
2023-09-21 15:43:23 +0200

    Directive Standalone, FIRST DEMO (shared module could be removed also in the first commit, because we import in the component, and its not used anywhere else )

--- src/app/welcome/details/details.component.ts
+++ src/app/welcome/details/details.component.ts
@@ -1,11 +1,10 @@
 import {Component} from '@angular/core';
 import {AnalyticsService} from 'src/app/shared/analytics.service';
 import {HighlightDirective} from "../../shared/highlight.directive";
-import {SharedModule} from "../../shared/shared.module";
 
 @Component({
     standalone: true,
-    imports: [SharedModule],  //** to import highlight directive
+    imports: [HighlightDirective],
     selector: 'app-details',
     templateUrl: './details.component.html',
     styleUrls: ['./details.component.css'],
