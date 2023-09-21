0bc33a528e31dabfba4a58a63bbe46019a3780b4
JurgenLubonja <jurgen.lubonja@eng.it>
2023-09-21 16:00:48 +0200

    2nd Version of application, components added, making fixes to use app routing module in standalone components

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,21 @@
-import { enableProdMode } from '@angular/core';
+import {enableProdMode, importProvidersFrom} from '@angular/core';
 
 import { environment } from './environments/environment';
 import {bootstrapApplication} from "@angular/platform-browser";
 import {AppComponent} from "./app/app.component";
 import {AnalyticsService} from "./app/shared/analytics.service";
+import {AppRoutingModule} from "./app/app-routing.module";
 
 if (environment.production) {
   enableProdMode();
 }
 
 bootstrapApplication(AppComponent,{
-  providers:[AnalyticsService]  //? Right way for providers
+  providers:[
+      AnalyticsService,
+      importProvidersFrom(AppRoutingModule), //* Theoretically Routing module is not a provider, but it
+                                            //* server as a provider in this case, this makes it recognizable for app component
+  ]
 })
 
 // platformBrowserDynamic().bootstrapModule(AppModule)
